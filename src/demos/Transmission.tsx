4639d76d143ee571a529f541a672479f2ea66374
Jackson Taylor <jackson@jksntaylor.com>
2022-01-13 09:56:51 -0700

    typescript fix

--- src/demos/Transmission.tsx
+++ src/demos/Transmission.tsx
@@ -1,37 +1,38 @@
-import React, { Suspense, useState, useEffect, useRef } from "react"
-import * as THREE from 'three'
+// import React, { Suspense, useState, useEffect, useRef } from "react"
+import React, { Suspense, useRef } from "react"
+// import * as THREE from 'three'
 import { Canvas, useFrame } from "@react-three/fiber"
 import styled from "styled-components"
-import Model from "../assets/models/ReformLogo"
-import { Environment, OrbitControls, useTexture } from "@react-three/drei"
+// import Model from "../assets/models/ReformLogo"
+import { Environment, OrbitControls } from "@react-three/drei"
 import { Mesh } from "three"
 
-const Image: React.FC<{}> = () => {
-  const texture1 = useTexture('https://images.unsplash.com/photo-1495069781661-dfeacdef0531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80')
+// const Image: React.FC<{}> = () => {
+//   const texture1 = useTexture('https://images.unsplash.com/photo-1495069781661-dfeacdef0531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80')
 
-  const texture2 = useTexture('https://images.unsplash.com/photo-1588007375246-3ee823ef4851?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
+//   const texture2 = useTexture('https://images.unsplash.com/photo-1588007375246-3ee823ef4851?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80')
 
-  const image = useRef()
+//   const image = useRef()
 
-  const [texture, setTexture] = useState(true)
+//   const [texture, setTexture] = useState(true)
 
-  const handleClick = () => {
-    setTexture(!texture)
-  }
+//   const handleClick = () => {
+//     setTexture(!texture)
+//   }
 
-  useEffect(() => {
-    if (image.current) {
-      // @ts-ignore
-      image.current.uTexture = texture ? texture1: texture2
-      // @ts-ignore
-      image.current.side = THREE.DoubleSide
-    }
-  }, [texture, texture1, texture2])
-  return <mesh position={new THREE.Vector3(0, 0, -100)} onClick={handleClick}>
-  <planeGeometry args={[500, 500, 100, 100]}/>
-  <pixelRiverImageMaterial ref={image}/>
-</mesh>
-}
+//   useEffect(() => {
+//     if (image.current) {
+//       // @ts-ignore
+//       image.current.uTexture = texture ? texture1: texture2
+//       // @ts-ignore
+//       image.current.side = THREE.DoubleSide
+//     }
+//   }, [texture, texture1, texture2])
+//   return <mesh position={new THREE.Vector3(0, 0, -100)} onClick={handleClick}>
+//   <planeGeometry args={[500, 500, 100, 100]}/>
+//   <pixelRiverImageMaterial ref={image}/>
+// </mesh>
+// }
 
 const Glass: React.FC<{}> = () => {
   const glassRef = useRef<Mesh>(null!)
@@ -42,7 +43,7 @@ const Glass: React.FC<{}> = () => {
   })
 
   return <mesh ref={glassRef}>
-    <octahedronGeometry args={[100, 0]} />
+    <icosahedronGeometry args={[100, 0]} />
     <meshPhysicalMaterial
       color="white"
       transmission={1}
